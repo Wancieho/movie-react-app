74c8a7eccb56d11fac4a1c85eaa35948bdac86bf
Wancieho <wancieho@gmail.com>
2023-08-16 16:15:33 +0200

    converting to json file for data & adding translation

--- src/app/components/Movies/index.tsx
+++ src/app/components/Movies/index.tsx
@@ -1,59 +1,42 @@
 "use client";
 
-import { Box, Grid } from "@mui/material";
+import { Box, CircularProgress, Grid, TextField } from "@mui/material";
 import Movie, { IMovie } from "../Movie";
 
+import jsonData from "../../data/movies.json";
 import { useState } from "react";
 
 const Movies = () => {
-  const jsonData = {
-    movies: [
-      {
-        title: "The Shawshank Redemption",
-        year: 1994,
-        genre: ["Crime", "Drama"],
-        director: "Frank Darabont",
-        actors: ["Tim Robbins", "Morgan Freeman"],
-        rating: 9.3,
-      },
-      {
-        title: "The Godfather",
-        year: 1972,
-        genre: ["Crime", "Drama"],
-        director: "Francis Ford Coppola",
-        actors: ["Marlon Brando", "Al Pacino", "James Caan"],
-        rating: 9.2,
-      },
-      {
-        title: "The Dark Knight",
-        year: 2008,
-        genre: ["Action", "Crime", "Drama"],
-        director: "Christopher Nolan",
-        actors: ["Christian Bale", "Heath Ledger", "Gary Oldman"],
-        rating: 9.0,
-      },
-      {
-        title: "Inception",
-        year: 2010,
-        genre: ["Action", "Adventure", "Sci-Fi"],
-        director: "Christopher Nolan",
-        actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
-        rating: 8.8,
-      },
-    ],
-  };
-
   const [movies] = useState<IMovie[]>(jsonData.movies);
+
+  const filter = (e: any) => {
+    console.info("e", e);
+  };
 
   return (
     <Grid container spacing={2}>
-      {movies?.map((movie) => (
-        <Grid item xl={3} md={6} sm={12} xs={12}>
-          <Box className="flex h-full">
-            <Movie data={movie} />
-          </Box>
-        </Grid>
-      ))}
+      {!movies && <CircularProgress />}
+      {movies && (
+        <>
+          <Grid item xs={12}>
+            <TextField label="Outlined" variant="outlined" onChange={filter} />
+          </Grid>
+          {movies.map((movie) => (
+            <Grid
+              key={JSON.stringify(movie)}
+              item
+              xl={3}
+              md={6}
+              sm={12}
+              xs={12}
+            >
+              <Box className="flex h-full">
+                <Movie data={movie} />
+              </Box>
+            </Grid>
+          ))}
+        </>
+      )}
     </Grid>
   );
 };
--- src/app/pages/Movies/index.tsx
+++ src/app/pages/Movies/index.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { Grid, Typography } from "@mui/material";
 
 import Movies from "@/app/components/Movies";
+import { useTranslation } from "react-i18next";
 
 const MoviesPage = () => {
+  const { t } = useTranslation();
+
   return (
     <Grid container padding={2}>
       <Grid item xs={12}>
         <Typography variant="h1" className="text-white">
-          Movies
+          {t("movies")}
         </Typography>
       </Grid>
       <Movies />
